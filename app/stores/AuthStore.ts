6e8a888a2af51dea2fae2439f281fe35ac025a7b
hhs0001 <heitorstein@gmail.com>
2024-10-07 20:17:02 -0300

    udapte navbar

--- app/stores/AuthStore.ts
+++ app/stores/AuthStore.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 import { account, ID } from "@/app/appwrite";
 import { Models, OAuthProvider } from "appwrite";
 
@@ -12,74 +13,89 @@ interface AuthState {
   resetPassword: (email: string) => Promise<void>;
   loginWithOAuth: (
     provider: OAuthProvider,
-    onSucces?: string,
+    onSuccess?: string,
     onFailure?: string,
     scopes?: string[]
   ) => Promise<void>;
 }
 
-export const useAuthStore = create<AuthState>()((set) => ({
-  user: null,
-  loading: false,
-  error: null,
+export const useAuthStore = create<AuthState>()(
+  persist(
+    (set) => ({
+      user: null,
+      loading: false,
+      error: null,
 
-  login: async (email: string, password: string) => {
-    set({ loading: true, error: null });
-    try {
-      await account.createEmailPasswordSession(email, password);
-      const user = await account.get();
-      set({ user, loading: false });
-    } catch (error) {
-      set({ error: (error as Error).message, loading: false });
-    }
-  },
+      login: async (email: string, password: string) => {
+        set({ loading: true, error: null });
+        try {
+          await account.createEmailPasswordSession(email, password);
+          const user = await account.get();
+          set({ user, loading: false });
+        } catch (error) {
+          set({ error: (error as Error).message, loading: false });
+        }
+      },
 
-  register: async (email: string, password: string, name: string) => {
-    set({ loading: true, error: null });
-    try {
-      await account.create(ID.unique(), email, password, name);
-      await useAuthStore.getState().login(email, password);
-    } catch (error) {
-      set({ error: (error as Error).message, loading: false });
-    }
-  },
+      register: async (email: string, password: string, name: string) => {
+        set({ loading: true, error: null });
+        try {
+          await account.create(ID.unique(), email, password, name);
+          await useAuthStore.getState().login(email, password);
+        } catch (error) {
+          set({ error: (error as Error).message, loading: false });
+        }
+      },
 
-  loginWithOAuth: async (
-    provider: OAuthProvider,
-    onSucces?: string,
-    onFailure?: string,
-    scopes?: string[]
-  ) => {
-    set({ loading: true, error: null });
-    try {
-      await account.createOAuth2Session(provider, onSucces, onFailure, scopes);
-      const user = await account.get();
-      set({ user, loading: false });
-    } catch (error) {
-      set({ error: (error as Error).message, loading: false });
-    }
-  },
+      loginWithOAuth: async (
+        provider: OAuthProvider,
+        onSuccess?: string,
+        onFailure?: string,
+        scopes?: string[]
+      ) => {
+        set({ loading: true, error: null });
+        try {
+          await account.createOAuth2Session(
+            provider,
+            onSuccess,
+            onFailure,
+            scopes
+          );
+          const user = await account.get();
+          set({ user, loading: false });
+        } catch (error) {
+          set({ error: (error as Error).message, loading: false });
+        }
+      },
 
-  logout: async () => {
-    set({ loading: true, error: null });
-    try {
-      await account.deleteSession("current");
-      set({ user: null, loading: false });
-    } catch (error) {
-      set({ error: (error as Error).message, loading: false });
-    }
-  },
+      logout: async () => {
+        set({ loading: true, error: null });
+        try {
+          await account.deleteSession("current");
+          set({ user: null, loading: false });
+        } catch (error) {
+          set({ error: (error as Error).message, loading: false });
+        }
+      },
 
-  resetPassword: async (email: string) => {
-    set({ loading: true, error: null });
-    try {
-      await account.createRecovery(
-        email,
-        "https://pulse.freecorps.xyz/reset-password"
-      );
-      set({ loading: false });
-    } catch (error) {
-      set({ error: (error as Error).message, loading: false });
+      resetPassword: async (email: string) => {
+        set({ loading: true, error: null });
+        try {
+          await account.createRecovery(
+            email,
+            "https://pulse.freecorps.xyz/reset-password"
+          );
+          set({ loading: false });
+        } catch (error) {
+          set({ error: (error as Error).message, loading: false });
+        }
+      },
+    }),
+    {
+      name: "auth-storage",
+      partialize: (state) => ({
+        user: state.user,
+      }),
     }
-  },
-}));
+  )
+);
---
+++ components/navbar.tsx
@@ -0,0 +1,192 @@
+"use client";
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import { JSX, SVGProps } from "react";
+import { useAuthStore } from "@/app/stores/AuthStore";
+import { Avatar } from "./ui/avatar";
+import { AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
+import { ModeToggle } from "./modeTogle";
+import { LogOut, Settings, User } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+
+export default function Navbar() {
+  const { user, logout } = useAuthStore();
+
+  return (
+    <header className="flex h-20 w-full shrink-0 items-center px-4 md:px-6">
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button variant="outline" size="icon" className="lg:hidden">
+            <MenuIcon className="h-6 w-6" />
+            <span className="sr-only">Toggle navigation menu</span>
+          </Button>
+        </SheetTrigger>
+        <SheetContent side="left">
+          <Link href="#" className="mr-6 hidden lg:flex" prefetch={false}>
+            <MountainIcon className="h-6 w-6" />
+            <span className="sr-only">Acme Inc</span>
+          </Link>
+          <div className="grid gap-2 py-6">
+            <Link
+              href="#"
+              className="flex w-full items-center py-2 text-lg font-semibold"
+              prefetch={false}
+            >
+              Home
+            </Link>
+            <Link
+              href="#"
+              className="flex w-full items-center py-2 text-lg font-semibold"
+              prefetch={false}
+            >
+              About
+            </Link>
+            <Link
+              href="#"
+              className="flex w-full items-center py-2 text-lg font-semibold"
+              prefetch={false}
+            >
+              Services
+            </Link>
+            <Link
+              href="#"
+              className="flex w-full items-center py-2 text-lg font-semibold"
+              prefetch={false}
+            >
+              Contact
+            </Link>
+          </div>
+        </SheetContent>
+      </Sheet>
+      <div className="flex flex-row items-center justify-between m-auto gap-96">
+        <Link href="#" className="mr-6 hidden lg:flex" prefetch={false}>
+          <MountainIcon className="h-6 w-6" />
+          <span className="sr-only">Acme Inc</span>
+        </Link>
+        <nav className="ml-auto hidden lg:flex gap-6">
+          <Link href="" prefetch={false}>
+            <Button variant="ghost">Home</Button>
+          </Link>
+          <Link href="./about" prefetch={false}>
+            <Button variant="ghost">About</Button>
+          </Link>
+          <Link href="./services" prefetch={false}>
+            <Button variant="ghost">Services</Button>
+          </Link>
+          <Link href="./contact" prefetch={false}>
+            <Button variant="ghost">Contact</Button>
+          </Link>
+        </nav>
+        {user ? (
+          <div className="flex flex-row items-center gap-4">
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button
+                  variant="ghost"
+                  className="relative h-8 w-8 rounded-full"
+                >
+                  <Avatar className="h-8 w-8">
+                    <AvatarImage src={user.prefs.url} alt={user.name} />
+                    <AvatarFallback className="flex items-center justify-center m-auto">
+                      {user.name[0]}
+                    </AvatarFallback>
+                  </Avatar>
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="w-56" align="end" forceMount>
+                <DropdownMenuLabel className="font-normal">
+                  <div className="flex flex-col space-y-1">
+                    <p className="text-sm font-medium leading-none">
+                      {user.name}
+                    </p>
+                    <p className="text-xs leading-none text-muted-foreground">
+                      {user.email}
+                    </p>
+                  </div>
+                </DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuGroup>
+                  <DropdownMenuItem>
+                    <User className="mr-2 h-4 w-4" />
+                    <span>Profile</span>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    <Settings className="mr-2 h-4 w-4" />
+                    <span>Settings</span>
+                  </DropdownMenuItem>
+                </DropdownMenuGroup>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem onClick={logout}>
+                  <LogOut className="mr-2 h-4 w-4" />
+                  <span>Log out</span>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+            <ModeToggle />
+          </div>
+        ) : (
+          <div className="flex flex-row items-center gap-4">
+            <Link href="./auth" prefetch={false}>
+              <Button>Login</Button>
+            </Link>
+            <Link href="./auth" prefetch={false}>
+              <Button variant="outline">Register</Button>
+            </Link>
+            <ModeToggle />
+          </div>
+        )}
+      </div>
+    </header>
+  );
+}
+
+function MenuIcon(props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <line x1="4" x2="20" y1="12" y2="12" />
+      <line x1="4" x2="20" y1="6" y2="6" />
+      <line x1="4" x2="20" y1="18" y2="18" />
+    </svg>
+  );
+}
+
+function MountainIcon(
+  props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>
+) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="m8 3 4 8 5-5 5 15H2L8 3z" />
+    </svg>
+  );
+}
