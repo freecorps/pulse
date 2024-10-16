90325acb83fc5747940b7c73b75e6abe9a4d89ca
hhs0001 <heitorstein@gmail.com>
2024-10-16 18:47:26 -0300

    udpate na pagina de login

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import React from "react";
 import { OAuthProvider } from "appwrite";
 import { useAuthStore } from "../stores/AuthStore";
 import { z } from "zod";
@@ -10,13 +10,23 @@ import {
   CardDescription,
   CardTitle,
   CardHeader,
-  CardFooter,
 } from "@/components/ui/card";
 import { toast } from "sonner";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { FaDiscord, FaGoogle } from "react-icons/fa";
+
+const Separator: React.FC<{ children: React.ReactNode }> = ({ children }) => (
+  <div className="flex items-center my-4">
+    <div className="flex-grow border-t bg-primary"></div>
+    <span className="flex-shrink mx-4 text-sm">{children}</span>
+    <div className="flex-grow border-t bg-primary"></div>
+  </div>
+);
 
 export default function AuthPage() {
-  const { login, register, loading, user, loginWithOAuth, logout, error } =
+  const { login, register, loading, user, loginWithOAuth, logout } =
     useAuthStore();
 
   const loginSchema = z.object({
@@ -32,9 +42,26 @@ export default function AuthPage() {
 
   if (user) {
     return (
-      <div>
-        <h1>Welcome {user.email}</h1>
-        <button onClick={() => logout()}>Logout</button>
+      <div className="flex items-center justify-center min-h-screen">
+        <Card className="w-[340px]">
+          <CardHeader>
+            <CardTitle>Olá, {user.name}</CardTitle>
+            <CardDescription>Seja bem-vindo de volta!</CardDescription>
+          </CardHeader>
+          <CardContent className="flex gap-4 justify-between">
+            <Button
+              onClick={() => {
+                logout();
+                toast.success("Logout realizado com sucesso!");
+              }}
+            >
+              Fazer Logout
+            </Button>
+            <Button variant="secondary">
+              <Link href={"./dashboard"}>Ir para a Dashboard</Link>
+            </Button>
+          </CardContent>
+        </Card>
       </div>
     );
   }
@@ -58,6 +85,7 @@ export default function AuthPage() {
             </CardHeader>
             <CardContent className="space-y-2">
               <AutoForm
+                className="flex flex-col gap-1"
                 formSchema={loginSchema}
                 fieldConfig={{
                   password: {
@@ -76,6 +104,48 @@ export default function AuthPage() {
                 }}
               >
                 <AutoFormSubmit>Login</AutoFormSubmit>
+                <div>
+                  <Link href="/reset-password">
+                    <Button variant="secondary" className="w-full">
+                      Esqueci minha senha
+                    </Button>
+                  </Link>
+                </div>
+                <Separator>ou entre com</Separator>
+                <div className="flex gap-4 h-full justify-center mr-16 ml-16">
+                  <Button
+                    className="flex-grow"
+                    onClick={() => {
+                      loginWithOAuth(
+                        OAuthProvider.Discord,
+                        window.location.href
+                      );
+                    }}
+                  >
+                    <FaDiscord />
+                  </Button>
+                  <Button
+                    className="flex-grow"
+                    onClick={() => {
+                      loginWithOAuth(
+                        OAuthProvider.Google,
+                        window.location.href
+                      );
+                    }}
+                  >
+                    <FaGoogle />
+                  </Button>
+                </div>
+                <div className="text-sm">
+                  Ao clicar em "Login" você concorda com nossos{" "}
+                  <Link href="/terms" className="text-purple-500">
+                    Termos de Serviço
+                  </Link>{" "}
+                  e{" "}
+                  <Link href="/privacy" className="text-purple-500">
+                    Política de Privacidade
+                  </Link>
+                </div>
               </AutoForm>
             </CardContent>
           </Card>
@@ -90,6 +160,7 @@ export default function AuthPage() {
             </CardHeader>
             <CardContent className="space-y-2">
               <AutoForm
+                className="flex flex-col gap-1"
                 formSchema={registerSchema}
                 onSubmit={({ email, password, name }) => {
                   try {
@@ -100,6 +171,41 @@ export default function AuthPage() {
                 }}
               >
                 <AutoFormSubmit>Registrar</AutoFormSubmit>
+                <Separator>ou entre com</Separator>
+                <div className="flex gap-4 h-full justify-center mr-16 ml-16">
+                  <Button
+                    className="flex-grow"
+                    onClick={() => {
+                      loginWithOAuth(
+                        OAuthProvider.Discord,
+                        window.location.href
+                      );
+                    }}
+                  >
+                    <FaDiscord />
+                  </Button>
+                  <Button
+                    className="flex-grow"
+                    onClick={() => {
+                      loginWithOAuth(
+                        OAuthProvider.Google,
+                        window.location.href
+                      );
+                    }}
+                  >
+                    <FaGoogle />
+                  </Button>
+                </div>
+                <div className="text-sm">
+                  Ao clicar em "Registrar" você concorda com nossos{" "}
+                  <Link href="/terms" className="text-purple-500">
+                    Termos de Serviço
+                  </Link>{" "}
+                  e{" "}
+                  <Link href="/privacy" className="text-purple-500">
+                    Política de Privacidade
+                  </Link>
+                </div>
               </AutoForm>
             </CardContent>
           </Card>
