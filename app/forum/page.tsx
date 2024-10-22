b73358bfcf0af2f57fe2e4f80e4339aea8c95e31
ResT-0 <pereiramenis@gmail.com>
2024-10-22 17:32:14 -0300

    tela de forum 3.0

--- app/forum/page.tsx
+++ app/forum/page.tsx
@@ -3,6 +3,7 @@ import { ChevronRight } from 'lucide-react';
 import Footer from "@/components/footer";
 import Navbar from "@/components/navbar";
 import { Forumeffect } from "@/components/typewriter-effect";
+import { ExpandableCardDemo } from '@/components/forumlink';
 
 interface ForumItem {
   id: number;
@@ -21,28 +22,11 @@ export default function Forum() {
     <div className="min-h-screen flex flex-col">
       <Navbar />
       <div className="flex flex-col p-8">
-        <div className="flex flex-col mb-1">
         <div className="flex flex-col items-center">
         <Forumeffect />
-          <div className=" bg-muted rounded-lg p-4 size-10/12 items-center">
-            {items.map(item => (
-              <a 
-                key={item.id} 
-                href={item.link}
-                className="flex items-center justify-between mb-2 bg-primary-foreground rounded p-2 hover:bg-muted-foreground transition-colors"
-              >
-                <span className='flex-grow'>{item.text}</span>
-                <div className='flex items-center'>
-                <div className="flex-row-w-8 h-8 rounded-full flex items-center justify-center mr-3">
-                  <span className="font-bold">Data</span>
-                </div>
-                
-                <ChevronRight size={20} className="text-foreground" />
-                </div>
-              </a>
-            ))}
+          <div className=" bg-muted rounded-lg p-4 items-center size-8/12">
+          <ExpandableCardDemo />
           </div>
-        </div>
         </div>
       </div>
       <Footer />
