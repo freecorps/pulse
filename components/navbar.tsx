498f6af904a896dd210b8fd35c36fbeaba3c6764
hhs0001 <heitorstein@gmail.com>
2024-12-04 19:11:54 -0300

    ajuste do icone

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -27,9 +27,8 @@ export default function Navbar() {
       <div className="container h-full mx-auto px-4">
         <div className="flex items-center justify-between h-full">
           <div className="flex items-center gap-4">
-            <Link href="/" className="flex items-center" prefetch={false}>
-              <MountainIcon className="h-6 w-6" />
-              <span className="sr-only">Pulse</span>
+            <Link href="/" className="flex items-center gap-2" prefetch={false}>
+              <Logo className="h-7 w-7" />
             </Link>
             <nav className="hidden lg:flex items-center gap-4">
               {siteLinks.map((item) => (
@@ -150,23 +149,14 @@ function UserMenu({ user, logout }: UserMenuProps) {
   );
 }
 
-function MountainIcon(
-  props: React.JSX.IntrinsicAttributes & React.SVGProps<SVGSVGElement>
-) {
+function Logo(props: React.HTMLAttributes<HTMLImageElement>) {
   return (
-    <svg
+    <img
       {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="m8 3 4 8 5-5 5 15H2L8 3z" />
-    </svg>
+      src="/favicon.ico"
+      alt="Logo do site"
+      width={24}
+      height={24}
+    />
   );
 }
